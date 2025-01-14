f0301c7994e82bca5e721e2c7b4702c0f3458367
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2025-01-14 16:57:27 +0900

    HashSet

---
+++ src/inflearn_java_middle/collection/set/member/HashAndEqualsMain1.java
@@ -0,0 +1,24 @@
+package inflearn_java_middle.collection.set.member;
+
+import inflearn_java_middle.collection.set.MyHashSetV2;
+
+public class HashAndEqualsMain1 {
+
+    public static void main(String[] args) {
+        MyHashSetV2 set = new MyHashSetV2(10);
+        MemberOnlyHash m1 = new MemberOnlyHash("A");
+        MemberOnlyHash m2 = new MemberOnlyHash("A");
+        System.out.println("m1.hashCode() = " + m1.hashCode());
+        System.out.println("m2.hashCode() = " + m2.hashCode());
+        System.out.println("m1.equals(m2) = " + m1.equals(m2));
+
+        set.add(m1);
+        set.add(m2);
+        System.out.println(set);
+
+        memberNoHashNoEq searchValue = new memberNoHashNoEq("A");
+        System.out.println("searchValue.hashCode() = " + searchValue.hashCode());
+        boolean result = set.contains(searchValue);
+        System.out.println("result = " + result);
+    }
+}
---
+++ src/inflearn_java_middle/collection/set/member/Member.java
@@ -0,0 +1,35 @@
+package inflearn_java_middle.collection.set.member;
+
+import java.util.Objects;
+
+public class Member {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public Member(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Member member = (Member) object;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+}
