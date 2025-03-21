28a1828cffd98055129697df5a3e98a8a5d4f85d
Jinpyo An <dkswlsvy3312@gmail.com>
2025-03-21 15:34:31 +0900

    섹션 8. 네트워크 - 프로그램1

---
+++ src/inflearn_java_advanced2/network/tcp/v2/ClientV2.java
@@ -0,0 +1,45 @@
+package inflearn_java_advanced2.network.tcp.v2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static inflearn_java_advanced2.util.MyLogger.log;
+
+public class ClientV2 {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        log("클라이언트 시작");
+        final Socket socket = new Socket("localhost", PORT);
+        final DataInputStream input = new DataInputStream(socket.getInputStream());
+        final DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        log("소캣 연결: "+ socket);
+
+        final Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("전송 문자: ");
+            String toSend = scanner.nextLine();
+
+            output.writeUTF(toSend);
+            log("client -> server: "+ toSend);
+
+            if (toSend.equals("exit")) {
+                break;
+            }
+
+            final String received = input.readUTF();
+            log("client <- server: " + received);
+        }
+
+
+
+        log("연결 종료: "+ socket);
+        input.close();
+        output.close();
+        socket.close();
+    }
+}
