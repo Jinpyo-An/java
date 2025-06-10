6da14f4df1bf77e4e1bc5895529035a522a98bc8
Jinpyo An <dkswlsvy3312@gmail.com>
2025-06-10 16:33:30 +0900

    람다 vs 익명 클래스

---
+++ src/inflearn_java_advanced03/lambda/lambda6/OuterMain.java
@@ -0,0 +1,37 @@
+package inflearn_java_advanced03.lambda.lambda6;
+
+public class OuterMain {
+
+    private String message = "외부 클래스";
+
+    public void execute() {
+        Runnable anonymous = new Runnable() {
+
+            private String message = "익명 클래스";
+
+            @Override
+            public void run() {
+                System.out.println("[익명 클래스] this: " + this);
+                System.out.println("[익명 클래스] this.class: " + this.getClass());
+                System.out.println("[익명 클래스] this.message: " + this.message);
+            }
+        };
+
+        Runnable lambda = () -> {
+            System.out.println("[익명 클래스] this: " + this);
+            System.out.println("[익명 클래스] this.class: " + this.getClass());
+            System.out.println("[익명 클래스] this.message: " + this.message);
+        };
+
+        anonymous.run();
+        System.out.println("========================");
+        lambda.run();
+    }
+
+    public static void main(String[] args) {
+        final OuterMain outer = new OuterMain();
+        System.out.println("[외부 클래스]: "+ outer);
+        System.out.println("========================");
+        outer.execute();
+    }
+}
